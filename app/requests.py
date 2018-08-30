28988b3af2d1ab341234ad344b283c2cbb273be7
TonyKioko <tonykioko384@gmail.com>
2018-08-30 22:14:40 +0300

    get news_sources function added

--- app/requests.py
+++ app/requests.py
@@ -1,4 +1,61 @@
 from app import app
+import urllib.request,json
+from .models import source,article
+
+Source = source.Source
+Article = article.Article
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+# Getting the movie base url
+base_url = app.config["NEWS_API_BASE_URL"]
+
+def get_sources(source):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = "https://newsapi.org/v2/sources".format(source,api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results = None
+
+        if get_sources_response['results']:
+            source_results_list = get_sources_response['results']
+            source_results = process_results(source_results_list)
+
+
+    return source_results
+
+def process_results(source_list):
+    '''
+    Function  that processes the movie result and transform them to a list of Objects
+
+    Args:
+        movie_list: A list of dictionaries that contain movie details
+
+    Returns :
+        movie_results: A list of movie objects
+    '''
+    source_results = []
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        language = source_item.get('language')
+        country = source_item.get('country')
+
+        source_object = Source(id,name,description,url,category,language,country)
+        source_results.append(source_object)
+
+        # if name:
+        #     source_object = Source(id,name,description,url,category,language,country)
+            # source_results.append(source_object)
+        #    
+
+    return source_results
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .requests import get_sources
 
 # Views
 @app.route('/')
@@ -8,9 +9,9 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    message = 'Hello World'
+    news_sources = get_sources('sources')
     title = 'News Now'
-    return render_template('index.html',message=message,title=title)
+    return render_template('index.html',news_sources=news_sources,title=title)
 
 @app.route('/news/<source>')
 def movie(source):
