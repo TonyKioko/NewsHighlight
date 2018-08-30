04d15047e43a25d9584e9ed5f498e82f5d25f7e7
TonyKioko <tonykioko384@gmail.com>
2018-08-30 17:02:24 +0300

    new configuration file added

---
+++ app/views.py
@@ -0,0 +1,21 @@
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    message = 'Hello World'
+    title = 'News Now'
+    return render_template('index.html',message=message,title=title)
+
+@app.route('/news/<source>')
+def movie(source):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('index.html',source= source)
